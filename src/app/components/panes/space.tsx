3da00feee70e8c01536c803ec1ad38fa6ba722b8
KashimaKC <joshmaraun@gmail.com>
2023-12-22 13:07:12 -0800

    space page old additions

--- src/app/components/panes/space.tsx
+++ src/app/components/panes/space.tsx
@@ -1,9 +1,28 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { NASA_KEY } from "../../../.secret/API_KEYS"
+import Image from "next/image";
+import { space } from "@/app/styles/styles";
 
 const Space:FC = () => {
+
+    const [data, setData] = useState<any>()
+
+    useEffect(() => {
+        const gatherAPOD = async () => {
+            let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`)
+            setData(await response.json())
+        }
+
+        gatherAPOD()
+    }, [])
+
     return (
-        <div>
-            Space
+        <div style={space.spaceContainer as React.CSSProperties}>
+            <div style={{position: 'absolute', height: 500, width: 500}}>
+                {
+                    data !== undefined ? <Image src={data.hdurl} alt="" fill objectFit="contain" /> : <></>
+                }
+            </div>
         </div>
     )
 }
